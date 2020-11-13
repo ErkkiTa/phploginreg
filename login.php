7d3e25aefd4fee654edd95cda556747661c8cde5
ErkkiTa <erkkitamb5@gmail.com>
2020-11-13 16:31:40 +0200

    muudatused

--- login.php
+++ login.php
@@ -10,20 +10,17 @@
 require('db.php');
 session_start();
 
-if (isset($_POST['username'])){
-        
- $username = stripslashes($_REQUEST['username']);
-        
+if (isset($_POST['username'])){      
+ $username = stripslashes($_REQUEST['username']);    
  $username = mysqli_real_escape_string($con,$username);
  $password = stripslashes($_REQUEST['password']);
  $password = mysqli_real_escape_string($con,$password);
- 
-        $query = "SELECT * FROM `users` WHERE username='$username'
-and password='".md5($password)."'";
- $result = mysqli_query($con,$query) or die(mysql_error());
- $rows = mysqli_num_rows($result);
+    $query = "SELECT * FROM `users` WHERE username='$username'
+    and password='".md5($password)."'";
+$result = mysqli_query($con,$query) or die(mysql_error());
+$rows = mysqli_num_rows($result);
         if($rows==1){
-     $_SESSION['username'] = $username;
+    $_SESSION['username'] = $username;
             
      header("Location: index.php");
          }else{
